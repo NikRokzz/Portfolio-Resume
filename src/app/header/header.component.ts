deb33fc34cc1a4fb67d0aa25c69180ea197906c0
NikRokzz <nikhil.malewar@d2kindia.com>
2018-09-14 15:33:50 +0530

    Included library for typed.js.
    imported typed.js in header component.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as Typed from 'typed.js';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+
 
   constructor() { }
 
   ngOnInit() {
+    let headerName = new Typed('#headerName', {
+      strings: ['NIKHIL MALEWAR'],
+      typeSpeed: 40,
+      cursorChar: '',
+      backSpeed: 0
+    });
+    let headerDesc = new Typed('#headerDesc', {
+      strings: ['Web Designer, Developer and Game Addict...'],
+      typeSpeed: 40,
+      backSpeed: 0,
+      loop: true
+    });
   }
+
+  ngAfterViewInit() {
+
+  }
+
 
 }
